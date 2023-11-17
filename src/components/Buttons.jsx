5a33ca82443247f08491491ce7902bb8ffe38eed
Jonathan Byers <byersj920@gmail.com>
2023-11-16 20:06:01 -0600

    Buttons are now working again. Huzzah!

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,16 @@
+import satData from "./satData";
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
+  return (<div>
+  {displaySats.map((sat, id) => {
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-    </div>
+    <button onClick={() => filterByType(sat)} key={id}>
+      {sat} Orbit
+    </button>
   );
-};
+  })}
+  <button onClick={() => setSat(satData)}>This is a test</button>
+  </div>)
+}
 
 export default Buttons;
